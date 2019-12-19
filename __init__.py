87d6ca5b5c0ef59c6839c38cfdddcd58471004e9
Artalus <artalus-mail@yandex.ru>
2019-12-19 16:55:19 +0300

    hg: respect errors during original command and apply_phab_diff()

--- __init__.py
+++ __init__.py
@@ -3,18 +3,25 @@
 from mercurial import extensions
 from mercurial import commands
 from plugin import apply_phab_diff
+from logger import log
 
 
 def _update_with_diff(orig, ui, repo, *args, **kwargs):  # pragma: no cover
-    ret = orig(ui, repo, *args, **kwargs)
+    orig_failed = orig(ui, repo, *args, **kwargs)
+    if orig_failed:
+        return orig_failed
     repo_root = repo.url()
     if repo_root.startswith("file:"):
         repo_root = repo_root[5:]
     else:
         raise RuntimeError(
             "could not figure out repo location from '%s'" % repo_root)
-    apply_phab_diff(repo_root)
-    return ret
+    try:
+        apply_phab_diff(repo_root)
+    except Exception as e:
+        log(e, ui)
+        return True
+    return False
 
 
 def uisetup(_):  # pragma: no cover
