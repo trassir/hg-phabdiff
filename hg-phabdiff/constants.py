629ebae1cc8373760e2f6f8704ceb02372c2caa5
Vladimir Looze <woldemar@mimas.ru>
2020-11-09 19:11:27 +0300

    quotes cleanup

--- hg-phabdiff/constants.py
+++ hg-phabdiff/constants.py
@@ -12,8 +12,8 @@ def EXE_HG():
     hg_full_path = ""
     for hg_name in [os.path.sep + "hg.exe", os.path.sep + "hg"]:
         # importing pip is not supported by devs, they recommend this instead
-        mercurial_files = subprocess.check_output([sys.executable, '-m', 'pip', 'show', '-f', 'mercurial']).splitlines()
-        module_location = filter(lambda x: x.startswith('Location: '), mercurial_files)[0].split(': ')[1].strip()
+        mercurial_files = subprocess.check_output([sys.executable, "-m", "pip", "show", "-f", "mercurial"]).splitlines()
+        module_location = filter(lambda x: x.startswith("Location: "), mercurial_files)[0].split(": ")[1].strip()
         hg_paths = filter(lambda x, name=hg_name: x.endswith(name), mercurial_files)
         if hg_paths:
             hg_full_path = os.path.abspath(os.path.join(module_location, hg_paths[0].strip()))
