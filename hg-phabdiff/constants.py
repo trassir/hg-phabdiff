e4a1c68ed140fbd8da9154f432688ac52ae06562
Vladimir Looze <woldemar@mimas.ru>
2020-11-09 17:44:03 +0300

    add mercurial executable search
    resolves #5

--- hg-phabdiff/constants.py
+++ hg-phabdiff/constants.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python2
 
+import os
+import sys
+import subprocess
 
 def ENVVAR_PHAB_DIFF():
     return "HG_PHAB_DIFF"
 
 
 def EXE_HG():
-    return "hg"
+    if sys.platform == "win32":
+        hg_name = "\\hg.exe"
+    else:
+        hg_name  = "/hg"
+    # importing pip is not supported by devs, they recommend this instead
+    mercurial_files = subprocess.check_output([sys.executable, '-m', 'pip', 'show', '-f', 'mercurial']).splitlines()
+    module_location = filter(lambda x: x.startswith('Location: '), mercurial_files)[0].split(': ')[1].strip()
+    hg_path = filter(lambda x: x.endswith(hg_name), mercurial_files)[0].strip()
+    print(module_location)
+    print(hg_path)
+    return os.path.abspath(os.path.join(module_location, hg_path))
---
+++ hg-phabdiff/constants_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+
+import os
+import sys
+import subprocess
+from constants import EXE_HG
+
+
+def test_hg_found():
+    hg_exe_full_path = EXE_HG()
+    assert os.path.isfile(hg_exe_full_path)
+    assert os.path.exists(hg_exe_full_path)
+    file_str = subprocess.check_output(['file', hg_exe_full_path])
+    if sys.platform == 'win32':
+        assert 'PE32 executable (console) Intel 80386, for MS Windows' in file_str
+    else:
+        assert 'Python script, ASCII text executable' in file_str
