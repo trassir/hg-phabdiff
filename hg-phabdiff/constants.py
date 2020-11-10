f9cdeea74d691866625bb0cca437883e784b4e78
Vladimir Looze <woldemar@mimas.ru>
2020-11-10 13:14:42 +0300

    add mercurial executable search (#6)
    resolves #5

--- hg-phabdiff/constants.py
+++ hg-phabdiff/constants.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python2
 
+import os
+import sys
+import subprocess
 
 def ENVVAR_PHAB_DIFF():
     return "HG_PHAB_DIFF"
 
 
 def EXE_HG():
-    return "hg"
+    hg_full_path = ""
+    for hg_name in [os.path.sep + "hg.exe", os.path.sep + "hg"]:
+        # importing pip is not supported by devs, they recommend this instead
+        mercurial_files = subprocess.check_output([sys.executable, "-m", "pip", "show", "-f", "mercurial"]).splitlines()
+        module_location = filter(lambda x: x.startswith("Location: "), mercurial_files)[0].split(": ")[1].strip()
+        hg_paths = filter(lambda x, name=hg_name: x.endswith(name), mercurial_files)
+        if hg_paths:
+            hg_full_path = os.path.abspath(os.path.join(module_location, hg_paths[0].strip()))
+            break
+    return hg_full_path
---
+++ hg-phabdiff/constants_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+
+import os
+import subprocess
+from constants import EXE_HG
+
+
+def test_hg_found():
+    hg_exe_full_path = EXE_HG()
+    assert os.path.isfile(hg_exe_full_path)
+    assert os.path.exists(hg_exe_full_path)
+    file_str = subprocess.check_output(['file', hg_exe_full_path])
+    assert 'executable' in file_str
